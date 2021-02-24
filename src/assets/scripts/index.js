bb10d6f66199e66cc7b5f1b146520ae7608593ff
Toni Panacek <toni.panacek@gmail.com>
2021-02-24 11:11:35 +0100

    moved position of image on home page mobile"

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,5 @@
 const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
+
+const yearSpan = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+yearSpan.innerText = currentYear;
