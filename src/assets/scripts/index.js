6cf32e711d63f49b78eb30372b4b669dd25dc2c6
Toni Panacek <toni.panacek@gmail.com>
2023-02-21 21:22:27 +0100

    Add GA and contact form to gather submissions

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,3 +3,19 @@ const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
 const yearSpan = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
 yearSpan.innerText = currentYear;
+
+const contactForm = document.querySelector("#contact-form");
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  let myForm = event.target
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
+
+contactForm.addEventListener("submit", handleSubmit);
