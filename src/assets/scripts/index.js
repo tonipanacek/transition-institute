4bb41f72b0bf2fcb8a11c537b54468e0cbcdc86c
Toni Panacek <toni.panacek@gmail.com>
2023-02-23 11:45:34 +0100

    Upgrade node version

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,5 +1,3 @@
-const foo = (() => Promise.resolve().then(() => console.log('Hello world')))();
-
 const yearSpan = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
 yearSpan.innerText = currentYear;
